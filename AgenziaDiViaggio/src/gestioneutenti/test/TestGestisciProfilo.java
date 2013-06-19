58e8c07acdad70347a556e69dad5e9ae13aed961
Giacomo Marciani <giacomo.marciani@gmail.com>
2013-06-19 04:12:58 +0200

    Login: JSP e Servlet (Warning nelle import causati da differente versione Tomcat credo: ho sviluppato su Tomcat 7) gestioneutenti.model.*: traduzione EN -> IT

--- AgenziaDiViaggio/src/gestioneutenti/test/TestGestisciProfilo.java
+++ AgenziaDiViaggio/src/gestioneutenti/test/TestGestisciProfilo.java
@@ -1,5 +1,6 @@
 package gestioneutenti.test;
 
+import gestioneutenti.model.Login;
 import gestioneutenti.model.Utente;
 import gestioneutenti.view.BoundaryGestisciProfilo;
 
@@ -7,7 +8,7 @@ import java.awt.*;
 
 public class TestGestisciProfilo {
 	
-	private static final Utente SAMPLE_USER = new Utente("Giacomo", "Marciani", "Roma", "27 6 1990", "Uomo", "giacomo.marciani@gmail.com", "Amministratore", "giacomo.marciani", "password");
+	private static final Utente SAMPLE_USER = new Utente("Giacomo", "Marciani", "Roma", "27 6 1990", "Uomo", "giacomo.marciani@gmail.com", "Amministratore", new Login("giacomo.marciani", "password"));
 	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
