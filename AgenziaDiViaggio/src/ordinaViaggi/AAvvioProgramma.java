bcf17fdd63cd956a1d17bc83a25ab2d6c1ec7c06
Gambella <Gambella@160.80.84.190>
2013-06-21 18:33:56 +0200

    Inserito Avvio programma corretto

---
+++ AgenziaDiViaggio/src/ordinaViaggi/AAvvioProgramma.java
@@ -0,0 +1,19 @@
+package ordinaViaggi;
+
+import ordinaViaggi.boundaries.AABoundaryAvvio;
+
+/**
+ * 
+ * @author Gambella Riccardo
+ * Avvio del programma.
+ * Avvia la BoindaryAvvio.
+ *
+ */
+
+public class AAvvioProgramma 
+{	
+	public static void main(String[] args) 
+	{
+		new AABoundaryAvvio();	
+	}
+}
