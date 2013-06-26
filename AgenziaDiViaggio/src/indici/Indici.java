ded5c0b3f89ff60f662c47f5ee3a11a0eccfbfb4
LucaPaoli91 <luca.paoli2@gmail.com>
2013-06-26 15:15:46 +0200

    Primo commit della classe Indici.
    Autori Luca Paoli e Jessica Lucia

---
+++ AgenziaDiViaggio/src/indici/Indici.java
@@ -0,0 +1,36 @@
+/*
+ * Authors: Luca Paoli e Jessica Lucia
+ */
+
+package indici;
+
+public class Indici{
+	private Indici(){}
+	
+	public static double computaIndiciMetodoA(){
+		return computaIndiciSuTuttiIViaggi();
+	}
+
+	public static double computaIndiciMetodoB(){
+		return computaIndiciUltimoAnno();
+	}
+	
+	public static double computaIndiciMetodoC(){
+		return computaIndiciUltimaModifica();
+	}
+	
+	private static double computaIndiciSuTuttiIViaggi(){
+		//TODO
+		return 0.0;
+	}
+
+	private static double computaIndiciUltimoAnno(){
+		//TODO
+		return 0.0;
+	}
+
+	private static double computaIndiciUltimaModifica(){
+		//TODO
+		return 0.0;
+	}
+}
