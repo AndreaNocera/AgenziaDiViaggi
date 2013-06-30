a02a1e297b0cef55f020cd6f2a2c0babd1e4f38a
RemoSperlongano <remo.sperlongano@libero.it>
2013-06-30 04:06:39 +0200

    Ultimo commit

--- AgenziaDiViaggio/src/gestione_Catalogo/entity/Ambiente.java
+++ AgenziaDiViaggio/src/gestione_Catalogo/entity/Ambiente.java
@@ -11,10 +11,6 @@ public abstract class Ambiente extends ElementoIntermedio {
 	
 	public Ambiente(IDEsternoElemento idEsternoElemento){
 		super(idEsternoElemento);
-		
-		AmbienteDAO dao = AmbienteDAO.getIstanza();
-		int id = dao.insertAndReturnId(idEsternoElemento);
-		setID(id);
 	}
 
 	public Ambiente(Integer ID, IDEsternoElemento idEsternoElemento){
