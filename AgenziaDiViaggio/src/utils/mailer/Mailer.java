87e4b280dfd394393361391cb8f275c0b5b53fea
Giacomo Marciani <giacomo.marciani@gmail.com>
2013-07-03 20:24:16 +0200

    UTILS MAILER: Ho applicato il polimorfismo al Mailer, affinchè potesse adattarsi alla StandaloneApp e alla WebApp senza duplicazione di codice. Aggiornato il Context del server.

--- AgenziaDiViaggio/src/utils/mailer/Mailer.java
+++ AgenziaDiViaggio/src/utils/mailer/Mailer.java
@@ -13,57 +13,12 @@
 
 package utils.mailer;
 
-import java.util.Properties;
-
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-
-public final class Mailer {
+public interface Mailer {
 	
-	private static Mailer singletonMailer = null;
-	private static final String USERNAME = "giacomo.marciani.voyager@gmail.com";
-	private static final String MAIL = "giacomo.marciani.voyager@gmail.com";
-	private static final String PASSWORD = "ispwvoyager";
-
-	private Mailer() {}
-
-	public static synchronized Mailer getInstance() {
-		if (singletonMailer == null) {
-			singletonMailer = new Mailer();
-			return singletonMailer;
-		}
-		
-		return singletonMailer;
-	}
+	static final String MAIL = "giacomo.marciani.voyager@gmail.com";
+	static final String USERNAME = "giacomo.marciani.voyager@gmail.com";	
+	static final String PASSWORD = "ispwvoyager";
 	
-	public void inviaMail(String mailDestinatario, String oggetto, String messaggio) {
-		Properties props = new Properties();
-		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.starttls.enable", "true");
-		props.put("mail.smtp.host", "smtp.gmail.com");
-		props.put("mail.smtp.port", "587");
- 
-		Session session = Session.getInstance(props, new javax.mail.Authenticator() {
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication(USERNAME, PASSWORD);
-			}
-		  });
- 
-		try { 
-			Message message = new MimeMessage(session);
-			message.setFrom(new InternetAddress(MAIL));
-			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(mailDestinatario));
-			message.setSubject(oggetto);
-			message.setText(messaggio); 
-			Transport.send(message); 
-		} catch (MessagingException e) {
-			throw new RuntimeException(e);
-		}
-	}
+	void inviaMail(String mailDestinatario, String oggetto, String messaggio);
 
 }
