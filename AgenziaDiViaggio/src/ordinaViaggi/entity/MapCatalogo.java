05e0b049ed35a5f7100cd503cd52aa9c1799f9e7
LucaPaoli91 <luca.paoli2@gmail.com>
2013-07-06 09:50:05 +0200

    Autore: Luca Paoli
    Aggiunti i lock alle Mappe. Ricalcolati i SerialVersionUID delle
    suddette classi.

--- AgenziaDiViaggio/src/ordinaViaggi/entity/MapCatalogo.java
+++ AgenziaDiViaggio/src/ordinaViaggi/entity/MapCatalogo.java
@@ -6,6 +6,8 @@ package ordinaViaggi.entity;
 import ordinaViaggi.exception.MapException;
 
 import java.util.TreeMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /** 
  * <!-- begin-UML-doc -->
@@ -18,51 +20,72 @@ public class MapCatalogo<T extends ElementoCatalogo> extends TreeMap<String, T>{
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = -6165364327193703741L;
+	private static final long serialVersionUID = -6279381576443659194L;
+	private final Lock lock;
+	
 	public MapCatalogo(){
-		
+		lock = new ReentrantLock();
 	}
 	
 	/**
 	 * Inserisce la chiave istanziando un ElementoCatalogo.
 	 */
+	@SuppressWarnings("unchecked")
 	public void insertRecord(String key) {
 		// TODO Auto-generated method stub
-		if(!containsKey(key))
-		{	
+		if(!containsKey(key)){
+			lock.lock();
 			super.put(key, (T) new ElementoIntermedio());
+			lock.unlock();
 		}
 	}
 	
 	public void removeRecord(String key) throws MapException {
 		// TODO Auto-generated method stub
-		if(!containsKey(key))
-		{	
+		if(!containsKey(key))		{	
 			throw new MapException("Errore in rimozione. Chiave non presente.");
 		}
-		super.remove(key);
 		
+		lock.lock();
+		super.remove(key);
+		lock.unlock();
 	}
 	
+	@SuppressWarnings("unchecked")
 	public void insertElementoIntermedio(String key, ElementoIntermedio elemento){
 		// TODO Auto-generated method stub
-			if(!containsKey(key))
-			{	
+			if(!containsKey(key)){
+				lock.lock();
 				super.put(key,(T) elemento);
+				lock.unlock();
 			}
 	}
+	@SuppressWarnings("unchecked")
 	public void insertElementoFinale(String key, ElementoFinale elemento){
 		// TODO Auto-generated method stub
-			if(!containsKey(key))
-			{	
+			if(!containsKey(key)){
+				lock.lock();
 				super.put(key,(T) elemento);
+				lock.unlock();
 			}
 	}
 	
 	public ElementoFinale getElementoFinale(String key){
-		return (ElementoFinale)super.get(key);
+		ElementoFinale elemento = null;
+		
+		lock.lock();
+		elemento = (ElementoFinale)super.get(key);
+		lock.unlock();
+		
+		return elemento;
 	}
 	public ElementoIntermedio getElementoIntermedio(String key){
-		return (ElementoIntermedio)super.get(key);
+		ElementoIntermedio elemento = null;
+		
+		lock.lock();
+		elemento = (ElementoIntermedio)super.get(key);
+		lock.unlock();
+		
+		return elemento;
 	}
 }
--- AgenziaDiViaggio/src/ordinaViaggi/entity/MapPrenotazioni.java
+++ AgenziaDiViaggio/src/ordinaViaggi/entity/MapPrenotazioni.java
@@ -3,21 +3,27 @@ package ordinaViaggi.entity;
 import ordinaViaggi.exception.MapException;
 
 import java.util.TreeMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class MapPrenotazioni extends TreeMap<Integer, Prenotazione>{
-	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 2201576937326242829L;
 
+	private static final long serialVersionUID = -1606210623577413355L;
+	private final Lock lock;
+
+	public MapPrenotazioni(){
+		lock = new ReentrantLock();
+	}
+	
 	/**
 	 * Inserisce la chiave istanziando una Prenotazione.
 	 */
 	public void insertRecord(Integer key) {
 		// TODO Auto-generated method stub
 		if (!containsKey(key)) {
+			lock.lock();
 			super.put(key, new Prenotazione());
+			lock.unlock();
 		}
 	}
 
@@ -27,7 +33,9 @@ public class MapPrenotazioni extends TreeMap<Integer, Prenotazione>{
 	public void insertRecord(Integer key, Prenotazione prenotazione) {
 		// TODO Auto-generated method stub
 		if (!containsKey(key)) {
+			lock.lock();
 			super.put(key, prenotazione);
+			lock.lock();
 		}
 	}
 
@@ -40,7 +48,9 @@ public class MapPrenotazioni extends TreeMap<Integer, Prenotazione>{
 		if (!containsKey(key)) {
 			throw new MapException("Errore in rimozione. Chiave non presente.");
 		}
+		lock.lock();
 		super.remove(key);
+		lock.unlock();
 
 	}
 }
